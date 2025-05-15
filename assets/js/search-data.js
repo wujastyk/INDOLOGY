b771d99e56dd03dd5612ec42ea6be842140ae30e
wujastyk <wujastyk@gmail.com>
2025-05-15 23:24:21 +0000

    Deploying to gh-pages from @ wujastyk/sushrutaproject2.0@1ee1472caba25a59032fb7c58fb38532ae4ca41c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -207,7 +207,7 @@ ninja.data = [{
         title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
-          window.open("/sushrutaproject2.0//feed.xml", "_blank");
+          window.open("/sushrutaproject2.0/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
