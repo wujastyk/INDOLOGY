cd8a3668a0f1a6a1180034eef295b35dd1ac7be9
wujastyk <wujastyk@gmail.com>
2025-05-16 18:25:19 +0000

    Deploying to gh-pages from @ wujastyk/sushrutaproject2.0@c0b327bfc236a1c69e4b535520597c49ea580848 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,34 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/sushrutaproject2.0/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/sushrutaproject2.0/projects/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/sushrutaproject2.0/repositories/";
-          },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/sushrutaproject2.0/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/sushrutaproject2.0/teaching/";
-          },
         },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
