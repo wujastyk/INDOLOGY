39f39eeff654df9b03449878d3af7f3dc157ac01
wujastyk <wujastyk@gmail.com>
2025-05-26 22:22:02 +0000

    Deploying to gh-pages from @ sushrutaproject2/website@dc7b6668941566585d6a9ae44ff636a2aa977f0e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-the-compendium-of-suśruta-in-time-and-space",
-    title: "The Compendium of Suśruta in Time and Space",
+    id: "nav-home",
+    title: "home",
     section: "Navigation",
     handler: () => {
       window.location.href = "/website/";
