23c02a766b5df1c831766666fc90678546bf1588
wujastyk <wujastyk@gmail.com>
2025-05-26 19:47:37 +0000

    Deploying to gh-pages from @ sushrutaproject2/website@a3666a76755fdcc2aa0367a550999d9c389eb1e0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-x",
+    title: "X",
     section: "Navigation",
     handler: () => {
       window.location.href = "/website/";
